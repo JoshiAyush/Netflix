7ee2d8f44beaddfbc595c265bf43a8316f61ec3c
Ayush Joshi <ayush854032@gmail.com>
2021-01-24 19:08:05 +0530

    added firestore

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -2,8 +2,17 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth'
 
-const config = {};
+import { seedDatabase } from '../seed';
 
-const firebase = Firebase.initializeApp(config);
+const firebaseConfig = {
+    apiKey: "AIzaSyBwaM_jiBbb7aJEAOHyv2jTuUlsFGT9PYE",
+    authDomain: "netflix-77adb.firebaseapp.com",
+    projectId: "netflix-77adb",
+    storageBucket: "netflix-77adb.appspot.com",
+    messagingSenderId: "426270832945",
+    appId: "1:426270832945:web:a97e6c2c1ad528de09d810"
+};
+
+const firebase = Firebase.initializeApp(firebaseConfig);
 
 export { firebase };
