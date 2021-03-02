82e42f3ba86afe748ce59fdce335ea0008e83186
Ayush Joshi <ayush854032@gmail.com>
2021-03-02 11:30:10 +0530

    changed useState hook for categories to useContext

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,14 +6,17 @@ import { createContext } from "react";
 
 import { CardContainer } from "./style/Card.js";
 
+import { BrowseContext } from "../../pages/Browse/index.js";
+
 export const FeatureContext = createContext();
 
 function Card({ slides }) {
+    const [slideRows, setSlideRows] = useState([]);
+
     const [showFeature, setShowFeature] = useState(false);
     const [itemFeature, setItemFeature] = useState({});
 
-    const [category, setCategory] = useState("series");
-    const [slideRows, setSlideRows] = useState([]);
+    const { category, setCategory } = useContext(BrowseContext);
 
     useEffect(() => {
         setSlideRows(slides[category]);
@@ -24,7 +27,44 @@ function Card({ slides }) {
 
             <CardContainer>
 
-                <CardContainer.Group></CardContainer.Group>
+                <CardContainer.Group>
+
+                    {
+                        slideRows.map(slideItem => (
+                            <>
+                                <CardContainer.Title>{slideItem.title}</CardContainer.Title>
+
+                                <CardContainer.Entities>
+                                    {
+                                        slideItem.data.map(item => {
+                                            console.log("I am your item", item);
+                                        })
+                                    }
+                                    {
+                                        slideItem.data.map(item => (
+                                            <CardContainer.Item key={item.docId} item={item}>
+
+                                                <CardContainer.Image
+                                                    src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                                                />
+
+                                                <CardContainer.Meta>
+
+                                                    <CardContainer.Subtitle>{item.title}</CardContainer.Subtitle>
+
+                                                    <CardContainer.Text>{item.description}</CardContainer.Text>
+
+                                                </CardContainer.Meta>
+
+                                            </CardContainer.Item>
+                                        ))
+                                    }
+                                </CardContainer.Entities>
+                            </>
+                        ))
+                    }
+
+                </CardContainer.Group>
 
             </CardContainer>
 
