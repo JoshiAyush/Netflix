10d3577442153b01b932d65c55be65f8e48b3edb
Ayush Joshi <ayush854032@gmail.com>
2021-03-05 11:55:30 +0530

    added new component Player

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Card } from "./Card/index.js";
 export { default as Form } from "./Form/index.js";
+export { default as Player } from "./Player/index.js";
 export { default as Feature } from "./Feature/index.js";
 export { default as Loading } from "./Loading/index.js";
 export { default as OptForm } from "./Opt-Form/index.js";
@@ -7,7 +8,8 @@ export { default as Accordion } from "./Accordion/index.js";
 
 export { CardContainer } from "./Card/style/Card.js";
 export { FormContainer } from "./Form/style/Form.js";
+export { PlayerContainer } from "./Player/style/Player.js";
 export { FeatureContainer } from "./Feature/style/Feature.js";
-export { OptFormContainer } from "./Opt-Form/style/Opt-Form.js";
 export { LoadingContainer } from "./Loading/style/Loading.js";
+export { OptFormContainer } from "./Opt-Form/style/Opt-Form.js";
 export { AccordionContainer } from "./Accordion/style/Accordion.js";
