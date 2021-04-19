a546639d8ea6172b2abddac399e5591935dfe1a0
Ayush Joshi <ayush854032@gmail.com>
2021-04-19 11:12:10 +0530

    Document function IsUserRedirect and ProtectedRoute
    Given a much clear outline of what routes.js file does in netflix project.

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -3,6 +3,16 @@ import { Route } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 
 export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+    /**
+     * @function IsUserRedirect() redirects the user to the logged in path if user have already logged in.
+     * 
+     * @argument {Object} Object.user is the user object it is null if there is no logged in user data.
+     * @argument {String} Object.loggedInPath is the route to browse page if user exists.
+     * @argument {Object} Object.children is the children component that we need to return if user doesn't exist.
+     * @argument {Obejct} Object.rest we spread these additional properties over the Route component.
+     * 
+     * @return {Object} children if user doesn't exist otherwise if no case met return null.
+     */
     return (
         <Route
             {...rest}
@@ -20,6 +30,16 @@ export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
 }
 
 export function ProtectedRoute({ user, signedInPath, children, ...rest }) {
+    /**
+     * @function ProtectedRoute() protects the page that can only be visited by a authorized user.
+     * 
+     * @argument {Object} Object.user is the authorized user.
+     * @argument {String} Object.signedInPath is the signed in path where we need to redirect the user to if user doesn't exist.
+     * @argument {Object} Object.children is the react component (protected route) that we need to return if there is a authorized user.
+     * @argument {Object} Object.rest we spread these additional properties over the Route component.
+     * 
+     * @return {Object} protected route if user exists otherwise signed in path or null.
+     */
     return (
         <Route
             {...rest}
