09a75aef8d37dbe6b862a4906a86af0ce9a51df0
Ayush Joshi <ayush854032@gmail.com>
2021-04-20 16:05:44 +0530

    Test #4 <Card /> component
    enabling testing on the card component while deleting the onclick event on the <Item />
    component as it was making the test to throw an error.
    
    Note: The <Card /> component isn't fully tested yet, there still are fixes that need to
    be done on the <Card /> component and the test itself.

---
+++ src/__tests__/components/card.test.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import { CardContainer } from "../../components/index.js";
+
+const category = "series";
+
+const slideRows = [
+    {
+        title: 'Documentaries',
+        data: [
+            {
+                genre: 'documentaries',
+                maturity: '18',
+                slug: 'tiger-king',
+                description: 'Tiger King description',
+                id: '07adcf19-517f-41b4-8f8b-04fee694bce1',
+                title: 'Tiger King',
+                docId: 'IcRxJBanzE7nmIp2GMGb',
+            },
+        ],
+    },
+    {
+        title: 'Feel Good',
+        data: [
+            {
+                title: 'Juno',
+                genre: 'feel-good',
+                description: 'Juno description',
+                maturity: '0',
+                id: 'a938b5a6-fe25-4a06-8050-353bc7146ccd',
+                slug: 'juno',
+                docId: '4JDgdf5vE0K3YrW9ZnbP',
+            },
+        ],
+    }
+];
+
+describe("<Card />", () => {
+    it("renders the <Card /> with populated data", () => {
+        const { container, getByText } = render(
+            slideRows.map((slideItem) => (
+                <>
+                    <CardContainer.Title>{slideItem.title}</CardContainer.Title>
+
+                    <CardContainer.Entities>
+                        {
+                            slideItem.data.map((item) => (
+                                <CardContainer.Item key={item.docId} item={item} data-testid={`${item.slug}--feautre`}>
+
+                                    <CardContainer.Image
+                                        src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}
+                                    />
+
+                                    <CardContainer.Meta>
+
+                                        <CardContainer.Subtitle>{item.title}</CardContainer.Subtitle>
+
+                                        <CardContainer.Text>{item.description}</CardContainer.Text>
+
+                                    </CardContainer.Meta>
+
+                                </CardContainer.Item>
+                            ))
+                        }
+                    </CardContainer.Entities>
+                </>
+            ))
+        );
+
+        expect(getByText("Documentaries")).toBeTruthy();
+        expect(getByText("Tiger King")).toBeTruthy();
+        expect(getByText("Tiger King description")).toBeTruthy();
+
+        expect(getByText("Feel Good")).toBeTruthy();
+        expect(getByText("Juno")).toBeTruthy();
+        expect(getByText("Juno description")).toBeTruthy();
+
+        expect(container.firstChild).toMatchSnapshot();
+    });
+});
