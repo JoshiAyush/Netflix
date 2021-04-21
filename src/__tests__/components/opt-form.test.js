063c4bbcf50bf4b06349c04e01f1977d000e4367
Ayush Joshi <ayush854032@gmail.com>
2021-04-21 11:17:43 +0530

    PASS  src/__tests__/components/opt-form.test.js
    Component <OptForm /> was successfull, it renders the components with populated data.

---
+++ src/__tests__/components/opt-form.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import { OptFormContainer } from "../../components/index.js";
+
+describe("<OptForm />", () => {
+    it("renders the <OptForm /> with populated data", () => {
+        const { container, getByText, getByPlaceholderText } = render(
+            <OptFormContainer>
+
+                <OptFormContainer.Input placeholder="Email address" />
+
+                <OptFormContainer.Button width="32.8%">Try it now</OptFormContainer.Button>
+
+                <OptFormContainer.Text>
+
+                    Ready to watch? Enter your email to create or restart your membership
+
+                </OptFormContainer.Text>
+
+            </OptFormContainer>
+        );
+
+        expect(getByText("Try it now")).toBeTruthy();
+        expect(getByText("Ready to watch? Enter your email to create or restart your membership")).toBeTruthy();
+        expect(getByPlaceholderText("Email address")).toBeTruthy();
+
+        expect(container.firstChild).toMatchSnapshot();
+    });
+});
