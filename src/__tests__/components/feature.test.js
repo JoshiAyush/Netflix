0acea997f38259025e1f0c57ae66e4eaf7be5ddc
Ayush Joshi <ayush854032@gmail.com>
2021-04-21 10:33:31 +0530

    PASS  src/__tests__/components/feature.test.js
    <Feature /> gives expected results, it renders the <Feature /> with populated
    data.

---
+++ src/__tests__/components/feature.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import { Feature } from "../../components/index.js";
+
+describe("<Feature />", () => {
+    it("renders the <Feature /> with populated data", () => {
+        const { container, getByText } = render(
+            <Feature />
+        );
+
+        expect(getByText("Unlimited films, TV programmes and more.")).toBeTruthy();
+        expect(getByText("Watch anywhere. Cancel at any time.")).toBeTruthy();
+
+        expect(container.firstChild).toMatchSnapshot();
+    });
+});
