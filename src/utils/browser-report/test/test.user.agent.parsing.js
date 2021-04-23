f505d7c912fb38f864c9163ff0935288f13ba542
Ayush Joshi <ayush854032@gmail.com>
2021-04-23 15:01:10 +0530

    Feature Browser report
    Added new feature where we show user his platform information and browser report.
    This feature is to make the user's profile page full of public details that we can
    access.

---
+++ src/utils/browser-report/test/test.user.agent.parsing.js
@@ -0,0 +1,32 @@
+window.describe("User Agent Matching", function () {
+    expectedResults.forEach(function (item) {
+        var expected = item.expectedReport;
+        var message = "should correctly identify {device}";
+
+        message = message.replace("{device}", item.device);
+
+        window.it(message, function () {
+            var report = window.browserReportSync(item.userAgent);
+
+            // add some custom data to the report
+            if (report.browser.version) {
+                report.browser.majorVersion = report.browser.version.split(".")[0];
+            }
+
+            for (var key in expected.browser) {
+                try {
+                    report.browser[key].should.equal(expected.browser[key]);
+                } catch (err) {
+                    /* eslint-disable no-console */
+                    console.log(report);
+                    /* eslint-enable no-console */
+                    throw err;
+                }
+            }
+            report.os.name.should.equal(expected.os.name);
+
+            // handle os version being null
+            should(report.os.version).be.exactly(expected.os.version);
+        });
+    });
+});
