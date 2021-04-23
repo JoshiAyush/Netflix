7088448723648ecaccc409159a7bb6782f71d067
Ayush Joshi <ayush854032@gmail.com>
2021-04-23 20:21:20 +0530

    Rename source files
    renamed src/utils/browser-report/index.js => src/utils/browser/browser-report.js.
    separated getUid() function from source file src/seed.js.

--- src/seed.js
+++ src/seed.js
@@ -1,4 +1,5 @@
 import data from "./data/data.json";
+import { getUid } from "./utils/uuid";
 
 export function seedDatabase(firebase) {
     /**
@@ -8,33 +9,6 @@ export function seedDatabase(firebase) {
      * @argument firebase is the firebase object for firebase authentication. 
      */
 
-    function getUUID() {
-        /**
-         * @function getUUID() generates a universal unique identifier.
-         * 
-         * A universally unique identifier (UUID) is a 128-bit number used to identify information in computer systems. The term 
-         * globally unique identifier (GUID) is also used, typically in software created by Microsoft. When generated according to
-         * the standard methods, UUIDs are, for practical purposes, unique. 
-         */
-
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-            /**
-             * @function replace() is used to replace a part of the given string with some another string or a regular expression.
-             * The original string will remain unchanged.
-             * 
-             * @syntax string.replace(regex, string);
-             * @parameter regex is a regular expression.
-             * @parameter string is the string which will replace the content of the given string. 
-             * 
-             * @return a new string with replaced value.
-             */
-
-            const piece = (Math.random() * 16) | 0;
-            const elem = c === 'x' ? piece : (piece & 0x3) | 0x8;
-
-            return elem.toString(16);
-        });
-    }
 
     for (let target in data) {
         for (let element of data[target]) {
@@ -54,7 +28,7 @@ export function seedDatabase(firebase) {
              */
             firebase.firestore().collection(target).add({
                 ...element,
-                id: getUUID()
+                id: getUid()
             });
         }
     }
--- src/utils/path.js
+++ src/utils/path.js
@@ -3,12 +3,12 @@ export function isUrl(string) {
         return false;
 
     let pattern = new RegExp(
-        '^(https?:\\/\\/)?' +                                       // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +        // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' +                             // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +                         // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' +                                // query string
-        '(\\#[-a-z\\d_]*)?$', 'i'                                   // fragment locator
+        '^(https?:\\/\\/)?' +                                       /** Protocol. */
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +        /** Domain name. */
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +                             /** OR ip (v4) address. */
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +                         /** Port and path. */
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +                                /** Query string. */
+        '(\\#[-a-z\\d_]*)?$', 'i'                                   /** Fragment locator. */
     );
 
     return pattern.test(string);
