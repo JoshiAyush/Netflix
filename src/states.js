1e9a9f94a05beb56bb54b4fa5f15ae04f618a0b0
Ayush Joshi <ayush854032@gmail.com>
2021-04-25 17:05:58 +0530

    Separate module for App state
    separated app states in a separate module, this makes sense.

---
+++ src/states.js
@@ -0,0 +1,14 @@
+export const SignUpState = {
+    error: "",
+    userName: "",
+    userEmail: "",
+    usePassword: "",
+    userPassword: "",
+    suggestPassword: false,
+    passwordSuggested: false
+};
+
+export const BrowseState = {
+    searchTerm: "",
+    category: "series"
+};
