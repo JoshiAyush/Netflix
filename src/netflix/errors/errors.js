e9d5e4c1ca3c76f65a1c2846e9062d495ae95656
Ayush Joshi <ayush854032@gmail.com>
2021-05-29 13:56:34 +0530

    feat (Exception): use custom exception for exception handling
    use custom exception for handling firebase, form validation exception this way we get
    a better software design

--- src/netflix/errors/errors.js
+++ src/netflix/errors/errors.js
@@ -1,7 +1,51 @@
-class ValidationError extends Error {
-  constructor(message) {
+export class ValidationErrorException extends Error {
+  constructor(message = "ValidationError") {
     super(message);
-    this.name = "ValidationError";
-    this.stack = null;
+
+    if (Error.captureStackTrace)
+      Error.captureStackTrace(this, ValidationErrorException);
+
+    this.name = "ValidationErrorException";
+    this.data = new Date();
+    this.message = "ValidationErrorException";
+  }
+}
+
+export class ArgumentTypeMisMatchException extends Error {
+  constructor(message = "ArgumentTypeMisMatchException", ...args) {
+    super(`${message}: ${args.join(", ")}`);
+
+    if (Error.captureStackTrace)
+      Error.captureStackTrace(this, ArgumentTypeMisMatchException);
+
+    this.name = "ArgumentTypeMisMatchException";
+    this.date = new Date();
+    this.message = `ArgumentTypeMisMatchException: ${args.join(", ")}`;
+  }
+}
+
+export class PropertyDoesNotExistException extends Error {
+  constructor(message = "PropertyDoesNotExistException", name = "") {
+    super(`${message}: ${name}`);
+
+    if (Error.captureStackTrace)
+      Error.captureStackTrace(this, PropertyDoesNotExistException);
+
+    this.name = "PropertyDoesNotExistException";
+    this.date = new Date();
+    this.message = `PropertyDoesNotExistException: ${name}`;
+  }
+}
+
+export class UndefinedVariableException extends Error {
+  constructor(message = "UndefinedVariableException", name = "") {
+    super(`${message}: ${name}`);
+
+    if (Error.captureStackTrace)
+      Error.captureStackTrace(this, UndefinedVariableException);
+
+    this.name = "UndefinedVariableException";
+    this.date = new Date();
+    this.message = `UndefinedVariableException: ${name}`;
   }
 }
