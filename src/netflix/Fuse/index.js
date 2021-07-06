c78e1731b7584f2f1c743ac63e7a16e7f51363f1
Ayush Joshi <ayush854032@gmail.com>
2021-07-06 12:16:25 +0530

    docs (documentation): documentation
    add documentation to function getSearchedResults()

--- src/netflix/Fuse/index.js
+++ src/netflix/Fuse/index.js
@@ -2,8 +2,7 @@ import Fuse from "fuse.js";
 
 export function getSearchedResults(docs, { keys = [] }, pattern) {
   /**
-   * @function getSearchedResults() returns the results after doing some fuzzy searching on the documents given for the given
-   * pattern.
+   * @function getSearchedResults() returns the results after doing some fuzzy searching on the documents given for the given pattern.
    * @argument {Any} docs document to search in.
    * @argument {Array} Object.keys keys to include while searching if the document is an object.
    * @argument {String} pattern pattern to search for in the document.
